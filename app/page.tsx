320665ae773c13a304da95675344be0f2aecb534
Noah Gabriel <nogabriel2003@gmail.com>
2026-01-07 22:37:38 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,31 @@
+import ExploreBtn from "@/components/ExploreBtn";
+import EventCard from "@/components/EventCard";
+import events from "@/lib/constants";
+
+
+
+const Page = () => {
+    return (
+        <section>
+            <h1 className="text-center">The Hub for Every Dev <br /> Event You Can't Miss </h1>
+            <p className="text-center mt-5">Hackathons, Meetups and Conferences, All in One Place</p>
+
+            <ExploreBtn />
+
+            <div className="mt-20 space-y-7">
+                <h3>Featured Events</h3>
+
+                <ol className="events">
+                    {events.map((event) => (
+                        <li key={event.title}>
+                            <EventCard {...event} />
+                        </li>
+                    ))}
+                </ol>
+            </div>
+        </section>
+    )
+}
+export default Page
+
+// 1:31:51 Sekunden Course -> https://www.youtube.com/watch?v=I1V9YWqRIeI
